52612252923a4be51fdb7ace4226f497c5ef0799
Ahmed <a.oumghar@cdo-formation.fr>
2023-05-31 16:56:08 +0200

    finally some proper admin mode

--- admin/login.php
+++ admin/login.php
@@ -67,8 +67,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 
-render_admin('header', ['title' => 'Mon compte', 'script' => 'index.js']);
+
+
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Titre de la page</title>
+  
+  <!-- Styles Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
+  <!-- Scripts Bootstrap (facultatif) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+</div>
 <div class="container">
     <div class="row justify-content-center align-items-center" style="height: 100vh;">
         <div class="col-md-6">
@@ -91,6 +109,8 @@ render_admin('header', ['title' => 'Mon compte', 'script' => 'index.js']);
         </div>
     </div>
 </div>
+</div>
+
 
 <?php
 render_admin('footer');
