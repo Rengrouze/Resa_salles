fd21fc6e49a2e342701d4d1e13db05db15909de5
Elias Oumghar <eoumghar@gmail.com>
2023-12-07 09:48:02 +0100

    cropper

--- traitement-image/accueil_photo.php
+++ traitement-image/accueil_photo.php
@@ -1,5 +1,5 @@
 <?php
-//https://www.letecode.com/php-comment-recadrer-une-image-avant-de-la-telecharger-a-laide-de-cropper-js
+// https://youtu.be/MttSRUarA6g
 ?>
 
 <!DOCTYPE html>
@@ -8,124 +8,62 @@
     <title>PHP recadrer l'image avant telechargement</title>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.css" integrity="sha512-bs9fAcCAeaDfA4A+NiShWR886eClUcBtqhipoY5DM60Y1V3BbVQlabthUBal5bq8Z8nnxxiyb1wfGX2n76N1Mw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.js" integrity="sha256-CgvH7sz3tHhkiVKh05kSUgG97YtzYNnWt6OXcmYzqHY=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha256-WqU1JavFxSAMcLP2WIOI+GB2zWmShMI82mTpLDcqFUg=" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.css" integrity="sha256-jKV9n9bkk/CTP8zbtEtnKaKf+ehRovOYeKoyfthwbC8=" crossorigin="anonymous" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.js" integrity="sha256-CgvH7sz3tHhkiVKh05kSUgG97YtzYNnWt6OXcmYzqHY=" crossorigin="anonymous"></script>
 </head>
-<style type="text/css">
-    img {
-        display: block;
+
+<style>
+    img{
+        display : block;
+        width: auto;
+        height: auto;
         max-width: 100%;
+
+    }
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 900px;
+        width: 900px;
     }
 
-    .preview {
-        overflow: hidden;
-        width: 160px;
-        height: 160px;
-        margin: 10px;
-        border: 1px solid red;
-    }
-
-    .modal-lg {
-        max-width: 1000px !important;
+    #output{
+        margin: 0 5px;
+      
     }
 </style>
-
 <body>
-    <div class="container">
-        <h1>PHP recadrer l'image avant telechargement - Letecode</h1>
-        <form method="post">
-            <div class="form-group">
-                <input type="file" class="form-control image" name="image" >
-            </div>
-        </form>
-    </div>
+<?php
 
-    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">Recadrer l'image</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="img-container">
-                        <div class="row">
-                            <div class="col-md-8">
-                                <img id="image" src="https://avatars0.githubusercontent.com/u/3456749">
-                            </div>
-                            <div class="col-md-4">
-                                <div class="preview"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn btn-primary" id="crop">Recadrer</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    </div>
-    </div>
-    <script>
-    var $modal = $('#modal');
-    var image = document.getElementById('image');
-    var cropper;
-    <?PHP
-    //get the room id and the filename from the URL
-    $roomId = $_GET['roomId'];
-    $filename = $_GET['filename'];
-    $imagePath = '../public/images/room_images/' . $roomId . '/min/temp/' . $filename;
+// get in the url the roomId and the filename
+$roomId = $_GET['roomId'];
+$filename = $_GET['filename'];
 ?>
-    $(document).ready(function() {
-        // Récupérer l'URL de l'image depuis l'URL du site
-        var imageUrl = <?= $imagePath ?>;
-        
-        // Afficher l'image dans le modal
-        if (imageUrl) {
-            done(imageUrl);
-            $modal.modal('show');
-        }
+<div class="container">
+    <div>
+        <img src="/public/images/room_images/<?=$roomId?>/min/temp/<?=$filename?>" alt="" id="image">
+        <button id="cropImageButton"></button>
+    </div>
+    <img src="" alt="" id="output">
+</div>
 
-        function getUrlParameter(name) {
-            name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-            var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-            var results = regex.exec(location.search);
-            return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-        }
-    });
-
-    $("body").on("change", ".image", function(e) {
-        var files = e.target.files;
-        var done = function(url) {
-            image.src = url;
-            $modal.modal('show');
-        };
-        var reader;
-        var file;
-        var url;
-
-        if (files && files.length > 0) {
-            file = files[0];
-
-            if (URL) {
-                done(URL.createObjectURL(file));
-            } else if (FileReader) {
-                reader = new FileReader();
-                reader.onload = function(e) {
-                    done(reader.result);
-                };
-                reader.readAsDataURL(file);
-            }
-        }
-    });
+<script>
+const image = document.getElementById('image');
+const cropper = new Cropper(image, {
+    aspectRatio: 1 / 1,
+    crop(event) {
+        console.log(event.detail.x);
+        console.log(event.detail.y);
+        console.log(event.detail.width);
+        console.log(event.detail.height);
+        console.log(event.detail.rotate);
+        console.log(event.detail.scaleX);
+        console.log(event.detail.scaleY);
+    },
+});
+  
 </script>
-
 </body>
 </html>
